8a29924643d4740459ddca18880bd586f098c751
sjpaterson <110463915+sjpaterson@users.noreply.github.com>
2022-10-26 22:13:59 +0800

    Update image.py

--- bin/image.py
+++ bin/image.py
@@ -54,23 +54,24 @@ chans = metadata['CHANNELS'].split(',')
 scale = basescale / metadata['CENTCHAN']
 scale = round(scale, 8)
 
+
 # Calculate min uvw in metres
 minuvm = 234 * minuv / metadata['CENTCHAN']
 
 
 # Check whether the phase centre has already changed, if not shift the pointing centre to point straight up, which approximates minw without making the phase centre rattle around.
-chgcentreResult = subprocess.run('chgcentre ' + os.path.join(obsdir, obsid + '.ms'), shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+chgcentreResult = subprocess.run('chgcentre ' + measurementSet, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 if 'shift' not in chgcentreResult.stderr.decode('utf-8'):
     # Determine whether to shift the pointing centre to be more optimally-centred on the peak of the primary beam sensitivity.
     coords = calcPointing.calc_optimal_ra_dec(metafits).to_string("hmsdms")
-    subprocess.run('chgcentre ' + os.path.join(obsdir, obsid + '.ms') + ' ' + coords, shell=True, check=True)
+    subprocess.run('chgcentre ' + measurementSet + ' ' + coords, shell=True, check=True)
     # Shift to point straight up.
     subprocess.run('chgcentre -zenith -shiftback ' + measurementSet, shell=True, check=True)
 
 
 
 if not os.path.exists(os.path.join(obsdir, obsid + '_template.fits')):
-    subprocess.run('wsclean -abs-mem 50 -mgain 1.0 -nmiter 1 -niter 0 -name ' + os.path.join(obsdir, obsid) + '_template -size ' + str(imsize) + ' ' + str(imsize) + ' -scale ' + str(scale) + ' -pol XX -data-column DATA -channel-range 4 5 -interval 4 5 -nwlayers 48 "' + measurementSet + '"', shell=True, check=True)
+    subprocess.run('wsclean -abs-mem 50 -mgain 1.0 -nmiter 1 -niter 0 -name ' + obsid + '_template -size ' + str(imsize) + ' ' + str(imsize) + ' -scale ' + str(scale) + ' -pol XX -data-column DATA -channel-range 4 5 -interval 4 5 -nwlayers 48 "' + measurementSet + '"', shell=True, check=True, cwd=obsdir)
     subprocess.run('rm "' + os.path.join(obsdir, obsid + '_template-dirty.fits') + '"', shell=True)
     subprocess.run('mv "' + os.path.join(obsdir, obsid + '_template-image.fits') + '" "' + os.path.join(obsdir, obsid + '_template.fits') + '"', shell=True)
 
@@ -84,9 +85,11 @@ for n in range(0,4):
     cstart = chans[i]
     j = i + 5
     cend = chans[j]
-    subprocess.run('lookup_jones.py ' + obsid + ' _template.fits ' + obsid + '_000$' + str(n) + '- -c $cstart-$cend --wsclean_names', shell=True, check=True)
+
+    # Check if the first pol exists for n, if so likely the others do aswell.
+    if not os.path.exists(os.path.join(obsdir, obsid + '_000' + str(n) + '-XX-beam.fits')):
+            subprocess.run('lookup_jones.py ' + obsid + ' _template.fits ' + obsid + '_000' + str(n) + '- -c ' + str(cstart) + '-' + str(cend) + ' --wsclean_names --beam_path "' + os.path.join(projectdir, 'beamdata/gleam_jones.hdf5') + '"', shell=True, check=True, cwd=obsdir)
     for pol in pols:
-        subprocess.run('ln -s "' + os.path.join(obsdir, obsid + '_000' + str(n) + '-' + str(pol) + '-beam.fits') + '" "' + os.path.join(obsdir, obsid + '_deep-000' + str(n) + '-beam' + str(pol) + '.fits') + '"', shell=True)  
+        subprocess.run('ln -s "' + os.path.join(obsdir, obsid + '_000' + str(n) + '-' + str(pol) + '-beam.fits') + '" "' + os.path.join(obsdir, obsid + '_deep-000' + str(n) + '-beam-' + str(pol) + '.fits') + '"', shell=True)  
 
-
-subprocess.run('wsclean -abs-mem 50 -multiscale -mgain 0.85 -multiscale-gain 0.15 -nmiter 5 -niter 10000000 -reuse-primary-beam -apply-primary-beam -auto-mask ' + str(msigma) + ' -auto-threshold ' + str(tsigma) + ' -name ' + os.path.join(obsdir, obsid) + '_deep -size ' + str(imsize) + ' ' + str(imsize) + ' -scale ' + str(scale) + ' -weight briggs ' + str(robust) + ' -pol I -join-channels -channels-out 4 -save-source-list -fit-spectral-pol 2 -data-column DATA "' + measurementSet + '"', shell=True, check=True)
+subprocess.run('wsclean -abs-mem 50 -multiscale -mgain 0.85 -multiscale-gain 0.15 -nmiter 5 -niter 10000000 -reuse-primary-beam -apply-primary-beam -auto-mask ' + str(msigma) + ' -auto-threshold ' + str(tsigma) + ' -name ' + obsid + '_deep -size ' + str(imsize) + ' ' + str(imsize) + ' -scale ' + str(scale) + ' -weight briggs ' + str(robust) + ' -pol I -join-channels -channels-out 4 -save-source-list -fit-spectral-pol 2 -data-column DATA ' + obsid + '.ms', shell=True, check=True, cwd=obsdir)
