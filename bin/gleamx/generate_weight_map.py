7ebc3788a5c0f17081741495d169dd6f4481179b
sjpaterson <110463915+sjpaterson@users.noreply.github.com>
2022-10-27 23:14:04 +0800

    gleam-x import with moditications

---
+++ bin/gleamx/generate_weight_map.py
@@ -0,0 +1,39 @@
+from astropy.io import fits
+import numpy as np
+import sys
+
+old_method = False
+
+def genWeightMap(in_xx, in_yy, in_rms, out_weight):
+    # out_weight = in_xx.replace("-XX-beam.fits", "_weight.fits")
+
+    hdu_xx = fits.open(in_xx)
+    hdu_yy = fits.open(in_yy)
+    hdu_rms = fits.open(in_rms)
+
+    try:
+        bscale = hdu_rms[0].header["BSCALE"]
+    except IndexError:
+        bscale = 1.0
+
+    stokes_I = (hdu_xx[0].data + hdu_yy[0].data) / 2.0
+    shape = np.array(hdu_rms[0].data.shape)
+    cen = shape // 2
+    delta = np.ceil(shape * 0.05).astype(np.int)
+
+
+    # Use a central region of the RMS map to calculate the weight via inverse variance
+    cen_rms = bscale * np.nanmean(
+        hdu_rms[0].data[
+            cen[0] - delta[0] : cen[0] + delta[0], cen[1] - delta[1] : cen[1] + delta[1],
+        ]
+    )
+
+    if not old_method:
+        weight = stokes_I ** 2 / cen_rms ** 2
+    else:
+        weight = 1.0 / (cen_rms) ** 2
+
+    hdu_xx[0].data = weight * stokes_I
+    hdu_xx.writeto(out_weight)
+
--- bin/postImage.py
+++ bin/postImage.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import astropy.units as u
 import gleamx.mask_image as mask_image
+import gleamx.generate_weight_map as gwm
 from astropy.io import fits
 from astropy.coordinates import SkyCoord
 
@@ -48,20 +49,34 @@ if (metadata['CENTCHAN'] == 69) and (b < 10):
     minsrcs=50
 
 for subchan in subchans:
-    chanHdu = fits.open(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb.fits'))
+
+    # Some file definitions for the channel.
+    pb = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb.fits')
+    pbxx = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb-XX-beam.fits')
+    pbyy = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb-YY-beam.fits')
+    pbmask = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_mask.fits')
+    pb_orig = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_original.fits')
+    pb_warp = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp.fits')
+
+    chanHdu = fits.open(pb)
     chanHead = chanHdu[0].header
     chanHdu.close()
     if chanHead['BMAJ'] == 0:
         print('ERROR: Zero-Size PSF')
         exit(-1)
 
+    # Channel information for creating the weight maps.
+    chans = metadata['CHANNELS'].split(',')
+    if subchan == 'MFS':
+        chanStart = 0
+        chanEnd = 23
+    else:
+        n = int(subchan)
+        chanStart = n * 6
+        chanEnd = chanStart + 5
+
     # As the mask_image.py operation will destructively remove the pixels, create a backup or restore from backup when required.
     if not os.path.exists(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_comp.fits')):
-        pb = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb.fits')
-        pbxx = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb-XX-beam.fits')
-        pbyy = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb-YY-beam.fits')
-        pbmask = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_mask.fits')
-        pb_orig = os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_original.fits')
         
         if not os.path.exists(pb_orig):
             subprocess.run('cp -v "' + pb + '" "' + pb_orig + '"', shell=True)
@@ -73,21 +88,61 @@ for subchan in subchans:
                 subprocess.run('cp -v "' + pb_orig + '" "' + pb + '"', shell=True)
 
         # Generate a weight map for mosaicking
-        chans = metadata['CHANNELS'].split(',')
-        if subchan == 'MFS':
-            i = 0
-            j = 23
-        else:
-            n = int(subchan)
-            i = n * 6
-            j = i + 5
-
-        subprocess.run('lookup_beam.py ' + pb + ' ' + obsid + '_deep-' + subchan + '-image-pb-' + '-c "' + chans[i] + '-' + chans[j] + '"', shell=True, check=True)
+        subprocess.run('lookup_beam.py ' + pb + ' ' + obsid + '_deep-' + subchan + '-image-pb-' + '-c "' + chans[chanStart] + '-' + chans[chanEnd] + '"', shell=True, check=True, cwd=obsdir)
         mask_image.derive_apply_beam_cut(image=pb, xx_beam=pbxx, yy_beam=pbyy, apply_mask=True)
 
-        # Move into place the new masked image.
+        # Move the new masked image into place.
         subprocess.run('rm "' + pb + '" && mv "' + pbmask + '" "' + pb + '"', shell=True)
 
-        subprocess.run('BANE --cores 48 --compress --noclobber "' + pb + '"', shell=True, check=True)
-        subprocess.run('aegean --cores 1 --autoload --table="' + pb + '" "' + pb + '"', shell=True, check=True)
+        subprocess.run('BANE --cores 48 --compress --noclobber "' + pb + '"', shell=True, check=True, cwd=obsdir)
+        subprocess.run('aegean --cores 1 --autoload --table="' + pb + '" "' + pb + '"', shell=True, check=True, cwd=obsdir)
+        
+        
+    # Check that a sufficient number of sources were detected.
+    catHdu = fits.open(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_comp.fits'))
+    cat = catHdu[1].data
+    nsrc = len(cat)
+    catHdu.close()
 
+    if nsrc < minsrcs:
+        print('ERROR: Not enough sources detected.')
+        exit(-1)
+
+    radius = 50
+    freqq = str(int(round(chanHead['CRVAL3']/1e6)))
+
+    if not os.path.exists(os.path.join(obsdir, obsid + '_' + subchan + '_complete_sources_xm.fits')):
+        subprocess.run('fits_warp.py --incat "' + obsid + '_deep-' + subchan + '-image-pb_comp.fits" --refcat "' + POS_MODEL_CATALOGUE + '" --xm "' + obsid + '_' + subchan + '_complete_sources_xm.fits" --plot --ra1 ra --dec1 dec --ra2 RAJ2000 --dec2 DEJ2000 --infits "' + pb + '"', shell=True, check=True, cwd=obsdir)
+    
+    if not os.path.exists(pb_warp):
+        subprocess.run('fits_warp.py --incat "' + obsid + '_deep-' + subchan + '-image-pb_comp.fits" --refcat "' + POS_MODEL_CATALOGUE + '" --corrected "' + obsid + '_deep-' + subchan + '-image-pb_comp_warp-corrected.fits" --xm "' + obsid + '_' + subchan + '_fits_warp_xm.fits" --suffix warp --infits "' + pb + '" --ra1 ra --dec1 dec --ra2 RAJ2000 --dec2 DEJ2000 --plot --nsrcs 750 --vm 10 --progress --cores 24 --signal peak_flux_1 --enforce-min-srcs 100', shell=True, check=True, cwd=obsdir)
+
+    # Flux_warp dependency, match the image catalogue to the model table.
+    if not os.path.exists(os.path.join(obsdir, obsid + '_' + subchan + '_xm.fits')):
+        subprocess.run('match_catalogues "' + obsid + '_deep-' + subchan + '-image-pb_comp_warp-corrected.fits" "' + FLUX_MODEL_CATALOGUE + '" --separation "' + separation + '" --exclusion_zone "' + exclusion + '" --outname "' + obsid + '_' + subchan + '_xm.fits" --threshold 0.5 --nmax 1000 --coords ' + str(metadata['RA']) + ' ' + str(metadata['DEC']) + ' --radius "' + str(radius) + '" --ra2 "RAJ2000" --dec2 "DEJ2000" --ra1 "ra" --dec1 "dec" -F "int_flux" --eflux "err_int_flux" --localrms "local_rms"', shell=True, check=True, cwd=obsdir)
+
+    if not os.path.exists(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_scaled_cf_output.txt')):
+        subprocess.run('flux_warp "' + obsid + '_' + subchan + '_xm.fits" "' + obsid + '_deep-' + subchan + '-image-pb_warp.fits" --mode mean --freq "' + freqq + '" --threshold 0.5 --nmax 400 --flux_key "flux" --smooth 5.0 --ignore_magellanic --localrms_key "local_rms" --add-to-header --ra_key "RAJ2000" --dec_key "DEJ2000" --index "alpha" --curvature "beta" --ref_flux_key "S_200" --ref_freq 200.0 --alpha -0.77 --plot --cmap "gnuplot2" --update-bscale --order 2 --ext png --nolatex', shell=True, check=True, cwd=obsdir)
+
+    # Get the header info from the flux warped image.
+    warpHdu = fits.open(pb_warp)
+    warpHead = warpHdu[0].header
+    warpHdu.close()
+    factor = warpHead['BSCALE']
+
+
+    # The RMS and BKG maps will not have changed much from the ionospheric warping, therefore rename them and update BSCALE.
+    subprocess.run('mv "' + os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_rms.fits') + '" "' + os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_rms.fits') + '"', shell=True)
+    subprocess.run('mv "' + os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_bkg.fits') + '" "' + os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_bkg.fits') + '"', shell=True)
+    fits.setval(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_rms.fits'), 'BSCALE', value=factor)
+    fits.setval(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_bkg.fits'), 'BSCALE', value=factor)
+
+
+    # Rerun the source finding.
+    if not os.path.exists(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_comp.fits')):
+        subprocess.run('aegean --cores 1 --autoload --table="' + pb_warp + '" "' + pb_warp + '"', shell=True, check=True, cwd=obsdir)
+
+    # Generate a weight map for mosaicking.
+    if not os.path.exists(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_weight.fits')):
+        subprocess.run('lookup_beam.py "' + obsid + '" "_deep-' + subchan + '-image-pb_warp.fits" "' + obsid + '_deep-' + subchan + '-image-pb_warp-" -c "' + chans[chanStart] + '-' + chans[chanEnd] + '"', shell=True, check=True, cwd=obsdir)
+        gwm.genWeightMap(os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp-XX-beam.fits'), os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp-YY-beam.fits'), os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_rms.fits'), os.path.join(obsdir, obsid + '_deep-' + subchan + '-image-pb_warp_weight.fits'))
