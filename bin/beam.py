Fixed scaling issue when creating the Stokes I image.

bin/beam.py
@@ -1,7 +1,9 @@
 import os
 import math
-import subprocess
+import numpy as np
 from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
 
 def calcBeamSize(psfFile):
     # Import the PSF header to read the BMAJ and BMIN for the restoring beam.
@@ -28,3 +30,20 @@ def applyPB(obsid, subchan, pol, beam):
     obsHdu.writeto(outFits)
     obsHdu.close()
 
+# Calculate the location of the beam centre either from the beam.fits or weight.fits.
+def calcBeamCentre(beamFile):
+    if not os.path.exists(beamFile):
+        print(f'Error, {beamFile} does not exist.')
+        exit()
+
+    obsFits = fits.open(beamFile)
+    obsWCS = WCS(obsFits[0].header).celestial
+    obsImage = obsFits[0].data
+    obsFits.close()
+    if obsImage.ndim > 2:
+        obsImage = obsImage[0,0,:,:]
+
+    centPos = np.unravel_index(np.argmax(obsImage), obsImage.shape)    
+    centreBeam = SkyCoord.from_pixel(centPos[1], centPos[0], obsWCS)
+    
+    return centreBeam

bin/catCalcs.py
@@ -6,18 +6,71 @@ from astropy.io import fits
 from astropy.coordinates import SkyCoord
 
 
-# Compare the defined number of brightest sources to the GLEAM catalogue
-# to calculate the scaling factor A.
-#def calcA(matchedCatFile, numSources, freq):
-def calcA(matchedCatFile, freq):
+
+# Compare to a catalogue to calculate the scaling factor A.
+def calcA(name, matchedCatFile, freq, method='snr'):
 
     with fits.open(matchedCatFile) as obsHdu:
         matchedCat = pd.DataFrame(np.array(obsHdu[1].data).byteswap().newbyteorder())
+
     matchedCat.sort_values('flux', ascending=False, inplace=True)
 
     matchedCat['gleam_flux'] = matchedCat['S_200'] * (float(freq) / 200.0) ** matchedCat['alpha']
     matchedCat['flux_ratio'] = matchedCat['flux'] / matchedCat['gleam_flux']
 
-    A = matchedCat['flux_ratio'].mean()
-        
+    f = open(f'acalc_{name}.txt', 'w')
+
+    f.write(f'Count All: {len(matchedCat.index)}\n')
+    # Only keep sources above 5 sigma.
+    matchedCat = matchedCat[matchedCat['flux'] > 5 * matchedCat['eflux']]
+    f.write(f'Count 5sig: {len(matchedCat.index)}\n')
+
+    # Set weighting based on brightness.
+    matchedCat['SNR'] = matchedCat['flux'] / matchedCat['eflux']
+    matchedCat['weightBright'] = matchedCat['flux'] / matchedCat['flux'].max()
+    matchedCat['weightRMS'] = matchedCat['eflux'] / matchedCat['eflux'].max()
+    matchedCat['weightSNR'] = matchedCat['SNR'] / matchedCat['SNR'].max()
+
+
+    matchedCat.to_csv(f'matched_{name}.csv')
+
+    AWeightRMS = np.average(matchedCat['flux_ratio'], weights=matchedCat['weightRMS'])
+    AWeightSNR = np.average(matchedCat['flux_ratio'], weights=matchedCat['weightSNR'])
+    ANoWeight = np.average(matchedCat['flux_ratio'])
+    ABrightest = np.average(matchedCat['flux_ratio'].head(20))
+
+    f.write('\n')
+    if method == 'snr':
+        f.write('Using SNR')
+        A = AWeightSNR
+    elif method == 'all':
+        f.write('Using All')
+        A = ANoWeight
+    else:
+        f.write('Using SNR')
+        A = AWeightSNR
+
+    f.write('\n')
+    f.write(f'Weighted RMS: {AWeightRMS}\n')
+    f.write(f'Weighted SNR: {AWeightSNR}\n')
+    f.write(f'No Weight All: {ANoWeight}\n')
+    f.write(f'No Weight 20 Brightest: {ABrightest}\n')
+    f.close()
+
     return A
+
+
+# Reduce the catalogue to only isolated sources. Sources without another sounce within {distance} degrees.
+def reduceCat(inCat, outCat, distance=0.05):
+    print(f'Reducing {inCat} to only include isolated sources with a minimum separation distance of {distance} degrees.')
+    catHdu = fits.open(inCat)
+    catData = catHdu[1].data
+
+    cat = SkyCoord(ra=catData['ra']*u.degree, dec=catData['dec']*u.degree)  
+    idx, d2d, d3d = cat.match_to_catalog_sky(cat, nthneighbor=2)
+    reducedCat = catData[d2d > distance*u.degree]
+    print(f'Reduced catalogue from {len(cat)} sources to {len(reducedCat)} sources.')
+
+    print(f'Writting: {outCat}')
+    catHdu[1].data = reducedCat
+    catHdu.writeto(outCat, overwrite=True)